552b6a7e986eb06f9beba199041a99c188b1c28c
Dmitriy <korbanitskiy@gmail.com>
2021-10-08 00:10:59 +0300

    Create roman_to_int.py

---
+++ roman_to_int.py
@@ -0,0 +1,52 @@
+"""
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+"""
+
+
+def roman_to_int(roman):
+    romans = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+    total = 0
+    prev = 0
+    for symbol in roman[::-1]:
+        current = romans[symbol]
+        if prev <= current:
+            total += current
+        else:
+            total -= current
+
+        prev = current
+
+    return total
+
+
+if __name__ == "__main__":
+    assert roman_to_int("III") == 3
+    assert roman_to_int("IV") == 4
+    assert roman_to_int("IX") == 9
+    assert roman_to_int("LVIII") == 58
+    assert roman_to_int("MCMXCIV") == 1994
